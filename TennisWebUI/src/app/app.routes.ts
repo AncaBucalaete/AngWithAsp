8a44dd4ff18fe742db24d16ed5e1c30d8ff909d6
Anca Bucalaete <farcas_anca@yahoo.com>
2025-02-25 15:15:23 +0200

    Updates

--- TennisWebUI/src/app/app.routes.ts
+++ TennisWebUI/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
 import { PlayerListComponent } from './components/player-list/player-list.component';
 import { CoachesComponent } from './coaches/coaches.component';
@@ -6,6 +6,7 @@ import { ScheduleComponent } from './schedule/schedule.component';
 import { SettingsComponent } from './settings/settings.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './auth/login/login.component';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
--- TennisWebUI/src/main.ts
+++ TennisWebUI/src/main.ts
@@ -3,7 +3,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import {} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom, isDevMode } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -13,18 +13,21 @@ import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideEffects } from '@ngrx/effects';
 import { authReducer } from './app/auth/state/auth.reducer';
 import { AuthEffects } from './app/auth/state/auth.effects';
+import { provideEntityData, withEffects } from '@ngrx/data';
+import { entityConfig } from './app/entity-metadata';
+import { provideRouterStore } from '@ngrx/router-store';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule, MatDialogModule, BrowserAnimationsModule),
-    
     // ✅ Provide Store Properly
     provideStore({ auth: authReducer }),
     provideEffects(AuthEffects),
-    
     // ✅ Enable Store Devtools in Dev Mode
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
-  ]
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEntityData(entityConfig, withEffects()),
+    provideRouterStore()
+]
 }).catch(err => console.error(err));
